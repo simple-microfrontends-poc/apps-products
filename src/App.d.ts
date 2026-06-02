fdb8699298749582a6156dab52f33b253a650809
xis <xis@schowek.net>
2026-06-02 16:09:29 +0200

    repo cleanup

--- src/App.d.ts
+++
@@ -1,4 +0,0 @@
-import "./styles/index.css";
-declare function App(): import("react/jsx-runtime").JSX.Element;
-export default App;
-//# sourceMappingURL=App.d.ts.map
--- src/components/ProductList.d.ts
+++
@@ -1,3 +0,0 @@
-declare function Products(): import("react/jsx-runtime").JSX.Element;
-export default Products;
-//# sourceMappingURL=ProductList.d.ts.map
